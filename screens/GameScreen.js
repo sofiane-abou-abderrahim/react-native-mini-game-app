858b7f60b388d2235d100023336f520864d001f3
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-16 19:09:04 +0100

    Starting Work on the Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent’s Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* +
+        - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
